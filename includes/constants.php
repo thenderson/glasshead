a0aa9e19fee14f1c0e126389fe8f3f7c5d85c34b
thenderson <todd.hndrsn@gmail.com>
2016-03-27 16:49:47 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,7 @@
 <?php
  //Global constants
 define("DATABASE", "glasshead_db");
-define("PASSWORD", "gGS2S^Z#11"); // beware of escape-worthy characters in password.
+define("PASSWORD", "wGS2S-Z#"); // beware of escape-worthy characters in password.
 define("SERVER", "localhost");
 define("USERNAME", "root");
 ?>
